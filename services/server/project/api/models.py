1d4aaebc0ead0cff740ab562e02a487ecd20850a
Andrii <not4me2009@gmail.com>
2020-08-02 22:53:23 +0300

    update

--- services/server/project/api/models.py
+++ services/server/project/api/models.py
@@ -6,24 +6,42 @@ from sqlalchemy.sql import func
 from project import db
 
 
-class Book(db.Model):
+class Category(db.Model):
 
-    __tablename__ = 'books'
+    __tablename__ = 'categories'
+    __table_args__ = {"schema": "store"}
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.String(255), nullable=False)
-    author = db.Column(db.String(255), nullable=False)
-    read = db.Column(db.Boolean(), default=False, nullable=False)
+    name = db.Column(db.String(255), nullable=False)
 
-    def __init__(self, title, author, read):
-        self.title = title
-        self.author = author
-        self.read = read
+    def __init__(self, name):
+        self.name = name
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
+
+
+class Product(db.Model):
+
+    __tablename__ = 'products'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(255), nullable=False)
+    author = db.Column(db.String(255), nullable=False)
+    category_id = Column(db.Integer, ForeignKey('store.categories.id'), nullable=False)
+
+    def __init__(self, name, price, category_id):
+        self.name = title
+        self.price = price
+        self.category_id = category_id
 
     def to_json(self):
         return {
             'id': self.id,
             'title': self.title,
-            'author': self.author,
-            'read': self.read
+            'price': self.price,
+            'category_id': self.category_id
         }
---
+++ services/server/project/api/products.py
@@ -0,0 +1,50 @@
+import os
+
+from flask import Blueprint, jsonify, request
+
+from project.api.models import Product
+from project import db
+
+
+products_blueprint = Blueprint('products', __name__)
+
+
+@products_blueprint.route('/products', methods=['GET', 'POST'])
+def all_products():
+    if request.method == 'POST':
+        post_data = request.get_json()
+        name = post_data.get('name')
+        price = post_data.get('price')
+        category_id = post_data.get('category_id')
+        db.session.add(Product(title=name, author=price, read=category_id))
+        db.session.commit()
+    else:
+        response_object['products'] = [product.to_json() for product in Product.query.all()]
+    return jsonify(response_object)
+
+
+@products_blueprint.route('/products/ping', methods=['GET'])
+def ping():
+    return jsonify({
+        'status': 'success',
+        'message': 'pong!'
+    })
+
+
+@products_blueprint.route('/products/<product_id>', methods=['PUT', 'DELETE'])
+def single_product(product_id):
+    product = Product.query.filter_by(id=product_id).first()
+    if request.method == 'PUT':
+        post_data = request.get_json()
+        product.name = post_data.get('name')
+        product.price = post_data.get('price')
+        product.category_id = post_data.get('category_id')
+        db.session.commit()
+    if request.method == 'DELETE':
+        db.session.delete(product)
+        db.session.commit()
+    return jsonify(response_object)
+
+
+if __name__ == '__main__':
+    app.run()
